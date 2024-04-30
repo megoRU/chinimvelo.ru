8cde6bf1f283a5eca6802e6af74630d5b5893948
megoRU <savin.yura2014@gmail.com>
2024-04-30 13:32:00 +0300

    Fix

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,23 +1,21 @@
 import './MainPage.css'
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 function MainPage() {
     return (
         <>
-            <div className="homePage">
-                <h1>
-                    <span style={{display: 'block'}}>КАЧЕСТВЕННЫЙ И</span>
-                    <span style={{display: 'block'}}>ЧЕСТНЫЙ</span>
-                    <span style={{display: 'block'}}>РЕМОНТ</span>
-                    <span style={{display: 'block'}}>ВЕЛОСИПЕДОВ</span>
-                    <span style={{display: 'block'}}>ПО ДОСТУПНОЙ</span>
-                    <span style={{display: 'block'}}>ЦЕНЕ</span>
-                    {/*<hr></hr>*/}
-                    <br/>
-                    <Link to="/price" className="button">ПРАЙС ЛИСТ</Link>
-                </h1>
-
-
+            <div className="parentContainer">
+                <div className="homePage">
+                    <h1>
+                        <span style={{display: 'block'}}>КАЧЕСТВЕННЫЙ И</span>
+                        <span style={{display: 'block'}}>ЧЕСТНЫЙ РЕМОНТ</span>
+                        <span style={{display: 'block'}}>ВЕЛОСИПЕДОВ</span>
+                        <span style={{display: 'block'}}>ПО ДОСТУПНОЙ ЦЕНЕ</span>
+                        {/*<hr></hr>*/}
+                        <br/>
+                        <Link to="/price" className="button">ПРАЙС ЛИСТ</Link>
+                    </h1>
+                </div>
             </div>
         </>
     );
