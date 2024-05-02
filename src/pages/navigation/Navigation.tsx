61303c87c32036b72e41f420c8b376c381ea6449
megoRU <savin.yura2014@gmail.com>
2024-05-02 18:39:09 +0300

    Fix

--- src/pages/navigation/Navigation.tsx
+++ src/pages/navigation/Navigation.tsx
@@ -12,26 +12,29 @@ const Navigation = () => {
     };
 
     return (
-        <div className="navigationContainer">
-            <div className="centeredLinks">
+        <>
+
+            <div className="navigationContainer">
                 <div className="logoContainer">
                     <a href="/">
                         <img src={logoImage} alt="Logo"/>
                     </a>
                 </div>
-                <div className={`mobileMenuIcon ${isMobileMenuOpen ? 'open' : ''}`} onClick={toggleMobileMenu}>
-                    <div className="bar1"></div>
-                    <div className="bar2"></div>
-                    <div className="bar3"></div>
-                </div>
-                <div className={`linksContainer ${isMobileMenuOpen ? 'open' : ''}`}>
-                    <Link to="/main" onClick={toggleMobileMenu}>Главная</Link>
-                    <Link to="/about" onClick={toggleMobileMenu}>О нас</Link>
-                    <Link to="/price" onClick={toggleMobileMenu}>Прайс лист</Link>
-                    <Link to="/contact" onClick={toggleMobileMenu}>Контакты</Link>
+                <div className="centeredLinks">
+                    <div className={`mobileMenuIcon ${isMobileMenuOpen ? 'open' : ''}`} onClick={toggleMobileMenu}>
+                        <div className="bar1"></div>
+                        <div className="bar2"></div>
+                        <div className="bar3"></div>
+                    </div>
+                    <div className={`linksContainer ${isMobileMenuOpen ? 'open' : ''}`}>
+                        <Link to="/main" onClick={toggleMobileMenu}>Главная</Link>
+                        <Link to="/about" onClick={toggleMobileMenu}>О нас</Link>
+                        <Link to="/price" onClick={toggleMobileMenu}>Прайс лист</Link>
+                        <Link to="/contact" onClick={toggleMobileMenu}>Контакты</Link>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
